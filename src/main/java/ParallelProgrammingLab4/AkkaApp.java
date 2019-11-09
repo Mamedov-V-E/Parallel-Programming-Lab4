54b0c11cabfef71c067d0b3772a5acd74c770821
Mamedov-V-E <black_snake666@mail.ru>
2019-11-09 11:13:48 +0300

    Scripted auto-commit on change (2019-11-09 11:13:48) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/AkkaApp.java
+++ src/main/java/ParallelProgrammingLab4/AkkaApp.java
@@ -5,6 +5,6 @@ import akka.actor.ActorSystem;
 public class AkkaApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("testerSystem");
-        
+
     }
 }
