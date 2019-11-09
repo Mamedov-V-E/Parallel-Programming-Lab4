21b3a5e7a2d94e23465b163c8450a90ccc00adad
Mamedov-V-E <black_snake666@mail.ru>
2019-11-09 11:22:10 +0300

    Scripted auto-commit on change (2019-11-09 11:22:10) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/StoreActor.java
+++ src/main/java/ParallelProgrammingLab4/StoreActor.java
@@ -1,4 +1,14 @@
 package ParallelProgrammingLab4;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreActor extends AbstractActor {
+    private Map<String, String> store = new HashMap<>();
+    @Override
+    public Receive createReceive() {
+
+    }
 }
