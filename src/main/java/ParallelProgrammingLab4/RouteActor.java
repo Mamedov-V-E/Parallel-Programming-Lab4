209e342c3b56f2085e088bc861ca90ef66b173fa
Mamedov-V-E <black_snake666@mail.ru>
2019-11-12 16:28:14 +0300

    Scripted auto-commit on change (2019-11-12 16:28:14) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/RouteActor.java
+++ src/main/java/ParallelProgrammingLab4/RouteActor.java
@@ -7,12 +7,12 @@ import akka.actor.Props;
 import akka.routing.RoundRobinPool;
 
 public class RouteActor extends AbstractActor {
-    private final int 
+    private final int EXECUTERS_NUMBER = 5;
 
     private ActorRef store = getContext().actorOf(
             Props.create(StoreActor.class)
     );
     private ActorRef executersPool = getContext().actorOf(
-            new RoundRobinPool()
+            new RoundRobinPool(EXECUTERS_NUMBER)
     )
 }
