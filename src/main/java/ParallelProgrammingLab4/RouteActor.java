26e105a688793d56b8ecc4d586ef03a6fe4b3cb8
Mamedov-V-E <black_snake666@mail.ru>
2019-11-12 16:32:34 +0300

    Scripted auto-commit on change (2019-11-12 16:32:34) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/RouteActor.java
+++ src/main/java/ParallelProgrammingLab4/RouteActor.java
@@ -15,5 +15,5 @@ public class RouteActor extends AbstractActor {
     private ActorRef executersPool = getContext().actorOf(
             new RoundRobinPool(EXECUTERS_NUMBER)
             .props(Props.create(ExecuteActor.class))
-    )
+    );
 }
