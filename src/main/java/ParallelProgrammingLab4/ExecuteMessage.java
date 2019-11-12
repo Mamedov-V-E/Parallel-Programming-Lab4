3af3e28a468498849d34fdb7e7e064ed864eb251
Mamedov-V-E <black_snake666@mail.ru>
2019-11-12 16:11:13 +0300

    Scripted auto-commit on change (2019-11-12 16:11:13) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/ExecuteMessage.java
+++ src/main/java/ParallelProgrammingLab4/ExecuteMessage.java
@@ -3,5 +3,15 @@ package ParallelProgrammingLab4;
 public class ExecuteMessage {
     private String functionName;
     private String params;
-    
+
+    public String getFunctionName() {
+        return this.functionName;
+    }
+    public String getParams() {
+        return this.params;
+    }
+
+    public ExecuteMessage(String functionName, String params) {
+        
+    }
 }
