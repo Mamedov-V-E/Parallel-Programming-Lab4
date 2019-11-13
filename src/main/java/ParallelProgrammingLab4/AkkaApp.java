d83496e1f3b680751a9e1035f0e595c1f499b2a6
Mamedov-V-E <black_snake666@mail.ru>
2019-11-13 11:14:10 +0300

    Scripted auto-commit on change (2019-11-13 11:14:10) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/AkkaApp.java
+++ src/main/java/ParallelProgrammingLab4/AkkaApp.java
@@ -1,11 +1,16 @@
 package ParallelProgrammingLab4;
 
 import ParallelProgrammingLab4.StoreActor.StoreActor;
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.net.http.HttpRequest;
 
 public class AkkaApp {
     public static void main(String[] args) {
@@ -15,6 +20,8 @@ public class AkkaApp {
         );
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        MainHttp instance = new MainHttp(system);
+        //MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                Http.cre
     }
 }
