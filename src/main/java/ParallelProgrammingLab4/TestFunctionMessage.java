4ed0a693e13e233e9fd77e68761d584e182b5493
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 00:44:15 +0300

    Scripted auto-commit on change (2019-11-23 00:44:15) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/TestFunctionMessage.java
+++ src/main/java/ParallelProgrammingLab4/TestFunctionMessage.java
@@ -1,5 +1,10 @@
 package ParallelProgrammingLab4;
 
 public class TestFunctionMessage {
-    
+    private String packageId;
+    private String jsScript;
+    private String functionName;
+    private String[] tests;
+
+    public TestFunctionMessage(String)
 }
