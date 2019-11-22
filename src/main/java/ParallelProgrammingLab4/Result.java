6a403a298d774a063e2cb62fc075cc5e27025749
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 01:35:01 +0300

    Scripted auto-commit on change (2019-11-23 01:35:01) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/Result.java
+++ src/main/java/ParallelProgrammingLab4/Result.java
@@ -1,11 +1,14 @@
 package ParallelProgrammingLab4;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @JsonAutoDetect
 public class Result {
-    
+
+    @JsonIgnore
     private final String packageId;
     private final String testName;
-    private final boolean isSuccessful
+    private final boolean isSuccessful;
+    private
 }
