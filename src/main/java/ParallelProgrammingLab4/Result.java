5c9c90b6d5f3d0eeced99ab6d2afcd4597fe2578
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 01:30:38 +0300

    Scripted auto-commit on change (2019-11-23 01:30:38) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/Result.java
+++ src/main/java/ParallelProgrammingLab4/Result.java
@@ -2,5 +2,6 @@ package ParallelProgrammingLab4;
 
 public class Result {
     private final String packageId;
-    private final String testName 
+    private final String testName;
+    private final boolean isSuccessful
 }
