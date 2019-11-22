5d27a9e5bd5a83c6ac2fd8a516613e39895a4d21
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 01:51:08 +0300

    Scripted auto-commit on change (2019-11-23 01:51:08) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/ExecuteActor.java
+++ src/main/java/ParallelProgrammingLab4/ExecuteActor.java
@@ -8,7 +8,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 public class ExecuteActor extends AbstractActor {
-
+    private final
     public Receive createReceive() {
         return receiveBuilder().create()
                 .match(ExecuteTestMessage.class, m -> {
