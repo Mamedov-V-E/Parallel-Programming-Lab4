c9e5965936c66a6393028cd7f7611dc5ce9e4fcb
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 01:25:32 +0300

    Scripted auto-commit on change (2019-11-23 01:25:32) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/ExecuteActor.java
+++ src/main/java/ParallelProgrammingLab4/ExecuteActor.java
@@ -3,12 +3,19 @@ package ParallelProgrammingLab4;
 import akka.actor.AbstractActor;
 import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public class ExecuteActor extends AbstractActor {
 
     public Receive createReceive() {
         return receiveBuilder().create()
                 .match(ExecuteTestMessage.class, m -> {
-
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+                    engine.eval(m.getFunction().getJsScript());
+                    Invocable invocable = (Invocable) engine;
+                    return invocable.invokeFunction(m.getFunction().getFunctionName(), m.getTest().getParams()).toString();
                 }).build();
     }
 }
