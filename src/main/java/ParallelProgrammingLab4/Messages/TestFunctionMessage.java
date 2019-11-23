b77323d6bd3ac1476e607fc81547dc58cb33d1ad
Mamedov-V-E <black_snake666@mail.ru>
2019-11-23 12:00:19 +0300

    Scripted auto-commit on change (2019-11-23 12:00:19) by gitwatch.sh

--- src/main/java/ParallelProgrammingLab4/Messages/TestFunctionMessage.java
+++ src/main/java/ParallelProgrammingLab4/Messages/TestFunctionMessage.java
@@ -2,7 +2,9 @@ package ParallelProgrammingLab4.Messages;
 
 import ParallelProgrammingLab4.DataClasses.Function;
 import ParallelProgrammingLab4.DataClasses.Test;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
+@JsonAutoDetect
 public class TestFunctionMessage {
     private Function function;
     private Test[] tests;
